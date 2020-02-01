aa42d0f7b9365a7462e725112f5eef9ce7d24efb
Stephen <20730278+ssheld@users.noreply.github.com>
2020-02-01 12:40:14 -0500

    Added code to read user input data

--- app.js
+++ app.js
@@ -1,35 +1,77 @@
- 
+// Budget Controller
  var budgetController = (function() {
     
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+// Some code
 
  })();
 
+
+
+
+ // UI Controller
  var UIController = (function() {
 
-    // Some code
-
- })();
-
- var controller = (function(budgetCtrl, UICtrl) {
-
-    var z = budgetCtrl.publicTest(5);
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
 
     return {
-        anotherPublic: function() {
-            console.log(z);
+        getInput: function() {
+
+            return {
+                // Will be either inc (for income) or exp (for expense)
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
         }
     }
 
+ })();
+
+
+
+
+ // Global APP Controller
+ var controller = (function(budgetCtrl, UICtrl) {
+
+    var DOM = UICtrl.getDOMstrings();
+
+    var ctrlAddItem = function() {
+                
+        // 1. Get the field input data
+        var input = UIController.getInput();
+        console.log(input);
+
+        
+        // 2. Add the item to the budget controller
+
+        // 3. Add the new item to the UI
+        
+        // 4. Calculate the budget 
+
+        // 5. Display the budget on the UI
+
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(e) {
+
+        if (e.keyCode === 13 || e.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
+
  })(budgetController, UIController);
+
+
 
